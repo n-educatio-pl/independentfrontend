a6ca1a5efeb8d2af5d5fda5f596bd05a7b40c311
Michal Szymczak <mszymczak@n-educatio.pl>
2014-04-23 15:35:42 +0200

    Improved stability - puppet modules are installed using "puppet module install" command.

--- manifests/bootstrap.sh
+++ manifests/bootstrap.sh
@@ -3,10 +3,11 @@
 if [ ! -f /home/vagrant/.runforthefirsttime ]; then
     echo "It seems that this box is running for the first time!"
     apt-get update # we have to update this list in order to connect to the repositories
-    apt-get install -y ruby puppet  # we need them to provision machine
-    gem install librarian-puppet
+    apt-get install -y ruby puppet # we need them to provision machine
     cd /vagrant # directory in sync with the project root directory on the host
-    librarian-puppet install # install external puppet modules
+    puppet module install puppetlabs-apt -f -v 1.4.2
+    puppet module install puppetlabs-postgresql -f -v 3.3.3
+    puppet module install puppetlabs-nodejs -f -v 0.4.0
     touch modules/.gitkeep # brings back the file the has been removed by installation above not to alter git repository
     touch /home/vagrant/.runforthefirsttime # don't run it during the next launch
 fi
--- manifests/modules/symfony/manifests/php/configuration.pp
+++ manifests/modules/symfony/manifests/php/configuration.pp
@@ -3,10 +3,11 @@ class symfony::php::configuration {
     notify => Service["apache2"],
     context => "/files/etc/php5/apache2/php.ini",
     changes => ["set Date/date.timezone 'Europe/Warsaw'", "set PHP/xdebug.max_nesting_level 250"],
-    require => Package["apache2"],
+    require => Package["apache2", "php5"],
   }
   augeas { "set php cli configuration":
     context => "/files/etc/php5/cli/php.ini",
     changes => ["set Date/date.timezone 'Europe/Warsaw'", "set PHP/xdebug.max_nesting_level 250"],
+    require => Package["apache2", "php5"],
   }
 }
